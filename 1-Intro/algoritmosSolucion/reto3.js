1e3e27faf060e18e9e76da7bd2f8ad331e937f93
deminore <deminore@hotmail.com>
2022-07-22 18:09:09 -0500

    Se modíficó el archivo reto3.js; Se creo la carpeta 2-intro-web, dentro de ella se crearon las carpetas intro y HTML

--- 1-Intro/algoritmosSolucion/reto3.js
+++ 1-Intro/algoritmosSolucion/reto3.js
@@ -1,6 +1,22 @@
 /* RETO 3: Mario quiere averiguar si un número es divisible entre 7 y 8. 
 Ayuda a Mario a escribir una función que tome un número. Si el número
-es divisible ntre 7 y 8, el programa debe retornar verdadero. Si el 
+es divisible entre 7 y 8, el programa debe retornar verdadero. Si el 
 número es divisible entre 7 y 8, retornar falso */
 
 
+function Divisible (x) {
+    let num = x;
+    if (num % 7 === 0 && num % 8 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let numDiv = Divisible (56);
+console.log("¿Es divisible?")
+console.log(numDiv)
+
+let numDiv2 = Divisible (6);
+console.log("¿Es divisible?")
+console.log(numDiv2)
