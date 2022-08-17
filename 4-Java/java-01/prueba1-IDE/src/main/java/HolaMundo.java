5f6847a5f55eb64700b5c6d50bcbd55c09c812fe
deminore <deminore@hotmail.com>
2022-08-16 21:27:33 -0500

    2-intro -> Consulta - 4-Java -> Hola Mundo con Java

---
+++ 4-Java/java-01/prueba1-IDE/src/main/java/HolaMundo.java
@@ -0,0 +1,13 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+
+        String nombre = "Diana Elizabeth";
+        System.out.println("Saludos desde Java a: " + nombre);
+
+        // 2 String miVariable = "Hola Mundo";
+        // 2 System.out.println(miVariable);
+
+        // 1 System.out.println("Hola Mundo Java");
+
+    }
+}
