1bbcaf8f3190d818bdc6ad41573deb1c27783f74
deminore <deminore@hotmail.com>
2022-08-17 15:07:49 -0500

    4-Java

---
+++ 4-Java/java-01/ejemplo-jdk/Hola.java
@@ -0,0 +1,15 @@
+public class Hola
+{
+	// Esto es una clase del JDK Java
+
+	// String miVariable = "Hola mundo Java";
+
+	public static void main (String arg []){
+
+		// String miVariable = "Hola mundo Java Saludo a: ";
+		String miNombre = "Diana Elizabeth Minor";
+
+		// Declaración del método main
+		System.out.println("Hola saludos a: " + miNombre);
+	}
+}
