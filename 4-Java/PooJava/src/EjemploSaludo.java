af868dcd0e54c078c86231683ce74cbef06745d1
deminore <deminore@hotmail.com>
2022-08-23 19:33:08 -0500

    POO en Java

---
+++ 4-Java/PooJava/src/EjemploSaludo.java
@@ -0,0 +1,12 @@
+public class EjemploSaludo {
+    public static void main(String[] args) {
+        Saludo objSaludo;
+        objSaludo = new Saludo();
+        objSaludo.saludar();
+
+        Saludo objSaludo0 = new Saludo();
+
+        System.out.println(objSaludo.saludar0());
+        System.out.println("Este es otro objeto -> " + objSaludo0.saludar0());
+    }
+}
---
+++ 4-Java/PooJava/src/Saludo.java
@@ -0,0 +1,12 @@
+public class Saludo {
+    void saludar (){
+        System.out.println("Hola Java desde OOP");
+    }
+
+    String saludar0(){
+        String textoS = "Hola mundo Java Oop";
+        return textoS;
+    }
+
+
+}
